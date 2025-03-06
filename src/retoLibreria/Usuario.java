3b6fa2c66be0ea7803313d559671152137bf6fc6
JonMon06 <jonmontefer@gmail.com>
2025-03-06 10:31:40 +0100

    Clase Usuario.java

--- src/retoLibreria/Usuario.java
+++ src/retoLibreria/Usuario.java
@@ -1,15 +1,15 @@
-package retoLibreria;
+ package retoLibreria;
 
 public class Usuario {
-	int idUsuario;
-    String dni;
-    String telefono;
-    String correoElectronico;
-    String nombreUsuario;
-    String contrasena;
-    String tipo; // Socio, Empleado
-    String numeroSeguridadSocial; // Solo empleados
-    String fechaRegistro;
+	private int idUsuario;
+    private String dni;
+    private String telefono;
+    private String correoElectronico;
+    private String nombreUsuario;
+    private String contrasena;
+    private String tipo; // Socio, Empleado
+    private String numeroSeguridadSocial; // Solo empleados
+    private String fechaRegistro;
     
     Usuario(int idUsuario, String dni, String telefono, String correoElectronico, String nombreUsuario, String contrasena, String tipo, String numeroSeguridadSocial, String fechaRegistro) {
         this.idUsuario = idUsuario;
@@ -27,4 +27,77 @@ public class Usuario {
     void actualizarDatos(String nuevoTelefono, String nuevoCorreo) {}
     void cambiarContrasena(String nuevaContrasena) {}
     void mostrarInformacion() {}
+
+	public int getIdUsuario() {
+		return idUsuario;
+	}
+
+	public void setIdUsuario(int idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getCorreoElectronico() {
+		return correoElectronico;
+	}
+
+	public void setCorreoElectronico(String correoElectronico) {
+		this.correoElectronico = correoElectronico;
+	}
+
+	public String getNombreUsuario() {
+		return nombreUsuario;
+	}
+
+	public void setNombreUsuario(String nombreUsuario) {
+		this.nombreUsuario = nombreUsuario;
+	}
+
+	public String getContrasena() {
+		return contrasena;
+	}
+
+	public void setContrasena(String contrasena) {
+		this.contrasena = contrasena;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getNumeroSeguridadSocial() {
+		return numeroSeguridadSocial;
+	}
+
+	public void setNumeroSeguridadSocial(String numeroSeguridadSocial) {
+		this.numeroSeguridadSocial = numeroSeguridadSocial;
+	}
+
+	public String getFechaRegistro() {
+		return fechaRegistro;
+	}
+
+	public void setFechaRegistro(String fechaRegistro) {
+		this.fechaRegistro = fechaRegistro;
+	}
+    
 }
