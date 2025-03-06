8d52353f2cecc9787f30b438e7bfa0977a1c2d01
JonMon06 <jonmontefer@gmail.com>
2025-03-06 10:29:07 +0100

    Nueva clase Libreria.java

--- src/retoLibreria/Libreria.java
+++ src/retoLibreria/Libreria.java
@@ -1,11 +1,36 @@
 package retoLibreria;
 
 public class Libreria {
-	   String nombre, direccion, telefono;
+	   private String nombre, direccion, telefono;
 	    
 	    public Libreria(String nombre, String direccion, String telefono) {
 	        this.nombre = nombre;
 	        this.direccion = direccion;
 	        this.telefono = telefono;
 	    }
+
+		public String getNombre() {
+			return nombre;
+		}
+
+		public void setNombre(String nombre) {
+			this.nombre = nombre;
+		}
+
+		public String getDireccion() {
+			return direccion;
+		}
+
+		public void setDireccion(String direccion) {
+			this.direccion = direccion;
+		}
+
+		public String getTelefono() {
+			return telefono;
+		}
+
+		public void setTelefono(String telefono) {
+			this.telefono = telefono;
+		}
+	    
 }
