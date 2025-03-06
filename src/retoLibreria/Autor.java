ad18c5fc0531cea39258f6ab8951e4d93c98de64
JonMon06 <jonmontefer@gmail.com>
2025-03-06 10:27:10 +0100

    Actualizado la clase Autor.java

--- src/retoLibreria/Autor.java
+++ src/retoLibreria/Autor.java
@@ -1,9 +1,9 @@
 package retoLibreria;
 
 public class Autor {
-	int idAutor;
-    String nombre;
-    String apellidos;
+	private int idAutor;
+    private String nombre;
+    private String apellidos;
     
     Autor(int idAutor, String nombre, String apellidos) {
         this.idAutor = idAutor;
@@ -14,4 +14,29 @@ public class Autor {
     void mostrarInformacion() {}
     void actualizarNombre(String nuevoNombre) {}
     void actualizarApellidos(String nuevosApellidos) {}
+
+	public int getIdAutor() {
+		return idAutor;
+	}
+
+	public void setIdAutor(int idAutor) {
+		this.idAutor = idAutor;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+    
 }
