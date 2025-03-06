a66924c8e45bb579beefadcd36e1130ec6cd776f
JonMon06 <jonmontefer@gmail.com>
2025-03-06 10:30:45 +0100

    Clase Prestamo.java

--- src/retoLibreria/Prestamo.java
+++ src/retoLibreria/Prestamo.java
@@ -1,12 +1,12 @@
 package retoLibreria;
 
 public class Prestamo {
-	 int idPrestamo;
-	    int idEjemplar;
-	    int idUsuario;
-	    String fechaPrestamo;
-	    int duracionPrestamo;
-	    String fechaDevolucion;
+	 private int idPrestamo;
+	    private int idEjemplar;
+	    private int idUsuario;
+	    private String fechaPrestamo;
+	    private int duracionPrestamo;
+	    private String fechaDevolucion;
 	    
 	    Prestamo(int idPrestamo, int idEjemplar, int idUsuario, String fechaPrestamo, int duracionPrestamo, String fechaDevolucion) {
 	        this.idPrestamo = idPrestamo;
@@ -21,4 +21,53 @@ public class Prestamo {
 	    void devolverEjemplar() {}
 	    void extenderPrestamo(int diasExtra) {}
 	    void mostrarDetallesPrestamo() {}
+
+		public int getIdPrestamo() {
+			return idPrestamo;
+		}
+
+		public void setIdPrestamo(int idPrestamo) {
+			this.idPrestamo = idPrestamo;
+		}
+
+		public int getIdEjemplar() {
+			return idEjemplar;
+		}
+
+		public void setIdEjemplar(int idEjemplar) {
+			this.idEjemplar = idEjemplar;
+		}
+
+		public int getIdUsuario() {
+			return idUsuario;
+		}
+
+		public void setIdUsuario(int idUsuario) {
+			this.idUsuario = idUsuario;
+		}
+
+		public String getFechaPrestamo() {
+			return fechaPrestamo;
+		}
+
+		public void setFechaPrestamo(String fechaPrestamo) {
+			this.fechaPrestamo = fechaPrestamo;
+		}
+
+		public int getDuracionPrestamo() {
+			return duracionPrestamo;
+		}
+
+		public void setDuracionPrestamo(int duracionPrestamo) {
+			this.duracionPrestamo = duracionPrestamo;
+		}
+
+		public String getFechaDevolucion() {
+			return fechaDevolucion;
+		}
+
+		public void setFechaDevolucion(String fechaDevolucion) {
+			this.fechaDevolucion = fechaDevolucion;
+		}
+	    
 }
